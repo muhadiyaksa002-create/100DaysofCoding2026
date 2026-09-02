9e45631955ce49646d402eeb9f00e357429e4a28
Muh.adiyaksa <muhadiyaksa002@gmail.com>
2026-09-02 21:10:22 +0800

    Create Day01.java

---
+++ Day01.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // Komentar satu baris
+        /* Komentar beberapa baris */
+        /** Komentar dokumentasi */
+
+        System.out.println("Hello, Dunia!!!");
+        System.out.println("Memahami struktur dasar kode Java & menggunakan semua jenis Komentar");
+    }
+}
