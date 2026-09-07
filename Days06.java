e4c3826cc07631f4761257569af8c51f300337b8
Muh.adiyaksa <muhadiyaksa002@gmail.com>
2026-09-07 15:26:32 +0800

    Days06.java

---
+++ Days06.java
@@ -0,0 +1,23 @@
+public class Days06 {
+    public static void main(String[] args) {
+        String Nama = "Muh.Adiyaksa";
+        char Grade = 'A';
+        boolean Status = true;
+
+        // char adalah tipe data primitif yang nilai nya di apit tanda kutip 1
+        // char adalah tipe data yang hanya menyimpan satu karakter
+
+        //boolean adalah tipe data primitif yang hanya bernilai benar dasan salah
+        // true and false
+        //boolean juga tidak perlu di apit oleh tanda kutip
+
+        System.out.println("===============================");
+        System.out.println("DATA MAHASISWA UNSULBAR");
+        System.out.println("===============================");
+        System.out.println("Nama\t\t:" + Nama);
+        System.out.println("Nilai Mahasiswa\t:" + Grade);
+        System.out.println("Status Mhs Aktif:" + Status);
+        System.out.println();
+        System.out.println("==========TERIMAKASIH==========");
+    }
+}
