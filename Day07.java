3caa5593d838a929d5e13f241447f8c42fee4129
Muh.adiyaksa <muhadiyaksa002@gmail.com>
2026-09-08 14:12:36 +0800

    Days07.java

---
+++ Day07.java
@@ -0,0 +1,29 @@
+public class Days07 {
+    public static void main(String[] args) {
+
+        // String tipe data non primitif yang menyimpan kumpulan karakter 
+        // String juga harus di apit olet tanda kutip 2 ""
+        // String sendiri harus di input menggunakan huruf kapital di awalnya
+
+        String nama = "Muh.Adiyaksa";
+        String nim = "H0725509";
+        String kelas = "PTI A 25";
+        String Prodi = "Pendidikan teknologi informasi";
+        String fakultas = "FKIP";
+        String alamat = "Lingk.salabulo";
+        String hobi = "Mancing dan runner";
+
+        System.out.println("===============================");
+        System.out.println("         BIODATA SAYA");
+        System.out.println("===============================");
+        System.out.println("Nama\t:" + nama);
+        System.out.println("NIM\t:" + nim);
+        System.out.println("Kelas\t:" + kelas);
+        System.out.println("Prodi\t:" + Prodi);
+        System.out.println("Fakultas:" + fakultas);
+        System.out.println("Alamat\t:" + alamat);
+        System.out.println("Hobi\t:" + hobi);
+        System.out.println();
+        System.out.println("==========TERIMAKASIH==========");
+    }
+}
