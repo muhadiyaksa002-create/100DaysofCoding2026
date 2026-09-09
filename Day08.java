a789d8fdc92522b7cabf9d3e60ebbdf50bfac67a
Muh.adiyaksa <muhadiyaksa002@gmail.com>
2026-09-09 18:59:01 +0800

    Day08.java

---
+++ Day08.java
@@ -0,0 +1,18 @@
+public class Days08 {
+    public static void main(String[] args) {
+        
+        // Deklarasi adalah proses membuat variabel dengan menentukan tpe data yang digunakan
+        //tipe int
+        int umur;
+
+        // inisialisasi adalah proses pemberian nilai awal pada variabel
+        umur =19;
+
+        System.out.println("Umur awal\t:" + umur);
+        
+        umur = 20;
+        // update variabel dari umur 19 ke 20
+        System.out.println("Umur setelah di update:" + umur);
+
+    }
+}
